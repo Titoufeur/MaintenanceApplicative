49de4f48f88a452be247b44e8d47b4c843b4e5f8
Titoufeur <titouan.letondal@gmail.com>
2025-03-10 14:44:28 +0100

    add constants

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,10 @@
 package com.gildedrose;
 
 class GildedRose {
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+    public static final String CONJURED_MANA_CAKE = "Conjured Mana Cake";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -10,15 +14,15 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             switch(items[i].name){
-                case "Aged Brie":
+                case AGED_BRIE:
                     handleBrie(items[i]);
                     break;
-                case "Backstage passes to a TAFKAL80ETC concert":
+                case BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT:
                     handleBackstage(items[i]);
                     break;
-                case "Sulfuras, Hand of Ragnaros":
+                case SULFURAS_HAND_OF_RAGNAROS:
                     break;
-                case "Conjured Mana Cake":
+                case CONJURED_MANA_CAKE:
                     handleConjured(items[i]);
                     break;
                 default:
